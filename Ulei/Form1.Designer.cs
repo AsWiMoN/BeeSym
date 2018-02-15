411ba5e73863d2177167848c036a8bb33de80395
srvr-2 <srvr-2@mtk.local>
2018-02-15 13:36:31 +0300

    Finish

--- Ulei/Form1.Designer.cs
+++ Ulei/Form1.Designer.cs
@@ -29,21 +29,21 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.workerBeeJob = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.assignJob = new System.Windows.Forms.Button();
+            this.shifts = new System.Windows.Forms.NumericUpDown();
             this.label2 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.workerBeeJob = new System.Windows.Forms.ComboBox();
+            this.nextShift = new System.Windows.Forms.Button();
             this.report = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shifts)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.assignJob);
+            this.groupBox1.Controls.Add(this.shifts);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.workerBeeJob);
@@ -53,6 +53,41 @@
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Присвоение заданий пчелам";
+            // 
+            // assignJob
+            // 
+            this.assignJob.Location = new System.Drawing.Point(6, 66);
+            this.assignJob.Name = "assignJob";
+            this.assignJob.Size = new System.Drawing.Size(266, 23);
+            this.assignJob.TabIndex = 2;
+            this.assignJob.Text = "Дать это задание пчеле";
+            this.assignJob.UseVisualStyleBackColor = true;
+            this.assignJob.Click += new System.EventHandler(this.assignJob_Click);
+            // 
+            // shifts
+            // 
+            this.shifts.Location = new System.Drawing.Point(152, 39);
+            this.shifts.Name = "shifts";
+            this.shifts.Size = new System.Drawing.Size(120, 20);
+            this.shifts.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Смены";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Работа";
             // 
             // workerBeeJob
             // 
@@ -70,49 +105,16 @@
             this.workerBeeJob.Size = new System.Drawing.Size(121, 21);
             this.workerBeeJob.TabIndex = 1;
             // 
-            // label1
+            // nextShift
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 23);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Работа";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(149, 23);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(42, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Смены";
-            // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(152, 39);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown1.TabIndex = 4;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(6, 66);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(266, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Дать это задание пчеле";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(303, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(128, 111);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Работать следующую смену";
-            this.button2.UseVisualStyleBackColor = true;
+            this.nextShift.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nextShift.Location = new System.Drawing.Point(303, 12);
+            this.nextShift.Name = "nextShift";
+            this.nextShift.Size = new System.Drawing.Size(128, 111);
+            this.nextShift.TabIndex = 2;
+            this.nextShift.Text = "Работать следующую смену";
+            this.nextShift.UseVisualStyleBackColor = true;
+            this.nextShift.Click += new System.EventHandler(this.button2_Click);
             // 
             // report
             // 
@@ -128,7 +130,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(438, 302);
             this.Controls.Add(this.report);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.nextShift);
             this.Controls.Add(this.groupBox1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -136,7 +138,7 @@
             this.Text = "Ulei";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shifts)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -145,12 +147,12 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button assignJob;
+        private System.Windows.Forms.NumericUpDown shifts;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox workerBeeJob;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button nextShift;
         private System.Windows.Forms.TextBox report;
     }
 }
--- Ulei/Form1.cs
+++ Ulei/Form1.cs
@@ -12,10 +12,34 @@ namespace Ulei
 {
     public partial class Form1 : Form
     {
+        private Queen queen;
         public Form1()
         {
             InitializeComponent();
-            work
+            workerBeeJob.SelectedIndex = 0;
+            Worker[] worker = new Worker[4];
+            worker[0] = new Worker(new string[] { "Сборщик нектара", "Изготовление меда" });
+            worker[1] = new Worker(new string[] { "Охрана яиц", "Обучение личинок" });
+            worker[2] = new Worker(new string[] { "Охрана улья", "Патруль" });
+            worker[3] = new Worker(new string[] { "Сборщик нектара", "Изготовление меда", "Охрана яиц", "Обучение личинок", "Охрана улья", "Патруль" });
+            queen = new Queen(worker);
+        }
+
+        private void assignJob_Click(object sender, EventArgs e)
+        {
+            if (queen.AssignWork(workerBeeJob.Text, (int)shifts.Value) == false)
+            {
+                MessageBox.Show("Для этого задания рабочих нет" + workerBeeJob.Text, "Матка говорит");
+            }
+            else
+            {
+                MessageBox.Show("Задание" + workerBeeJob.Text + " будет закончено через " + shifts.Value + " смен", "Матка говорит");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            report.Text = queen.WorkTheNextShift();
         }
     }
 }
